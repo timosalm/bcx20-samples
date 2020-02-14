2f7beb427bd14c451473bc2882bcc862ce051973
Timo Salm <tsalm@pivotal.io>
2020-02-14 14:05:19 +0100

    Add Spring Boot java maven sample code

---
+++ java-spring-boot-maven-sample/src/test/java/com/bosch/bic/samples/javaspringbootmavensample/SampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bosch.bic.samples.javaspringbootmavensample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
